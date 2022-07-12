b16a72c1c83d2e27c45ae01c9124881e86b537f5
Triple Baconator <triple@baconator.on.ca>
2022-07-12 12:55:42 -0400

    poll scm

--- src/slave1package/slave1class.java
+++ src/slave1package/slave1class.java
@@ -4,6 +4,7 @@ public class slave1class {
 
 	public static void main(String[] args) {
 		System.out.println("this is example for slave1");
+		System.out.println("this is example for poll scm");
 		// TODO Auto-generated method stub
 
 	}
