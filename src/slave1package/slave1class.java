c5c64c79118040cb259d6f9ab25e395dcb1adf0f
Triple Baconator <triple@baconator.on.ca>
2022-07-12 13:18:30 -0400

    webhook

--- src/slave1package/slave1class.java
+++ src/slave1package/slave1class.java
@@ -6,6 +6,7 @@ public class slave1class {
 		System.out.println("this is example for slave1");
 		System.out.println("this is example for poll scm");
 		System.out.println("example of webhook");
+		//hi
 		// TODO Auto-generated method stub
 
 	}
