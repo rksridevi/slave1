e6c29f0ff0d8b9be9dde3087924d69e7556492d3
Triple Baconator <triple@baconator.on.ca>
2022-07-12 09:10:43 -0400

    slave1

---
+++ src/slave1package/slave1class.java
@@ -0,0 +1,11 @@
+package slave1package;
+
+public class slave1class {
+
+	public static void main(String[] args) {
+		System.out.println("this is example for slave1");
+		// TODO Auto-generated method stub
+
+	}
+
+}
